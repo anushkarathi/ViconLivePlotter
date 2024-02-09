95902dab7fe19cca27e895d276ed8ae8ceefb597
Kevin Best <tkevinbest@gmail.com>
2024-02-09 08:34:09 -0500

    Added colors and symmetry line.

--- AMTI_BalanceBoard.py
+++ AMTI_BalanceBoard.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import colorsys as colors
 from matplotlib.animation import FuncAnimation
 
 from vicon import ViconSDK_Wrapper
@@ -17,11 +18,15 @@ class AMTI_BalanceBoardGUI:
         self.fig, self.ax = plt.subplots()
         plates = ['Left','Right']
         update = lambda frame : self.update_plot()
-        self.bars = self.ax.bar((0,1), (20,20))
+        init_total_force = 20
+        self.bars = self.ax.bar((0,1), (init_total_force,init_total_force))
         self.ax.set_ylim(0, 10)
         self.ax.set_xticks((0, 1))
         self.ax.set_xticklabels(plates)
         self.ax.set_ylabel('Force (N)')
+
+        # Draw a line showing symmetry
+        self.symmline = self.ax.axhline(init_total_force/2, linestyle='--')
 
         self.animation = FuncAnimation(self.fig, update, interval=100, cache_frame_data=False)
         plt.show()
@@ -31,11 +36,21 @@ class AMTI_BalanceBoardGUI:
         This method gets called by the animation to update the plot at a fixed rate.
         """
         AMTI_vals = self.viconServer.get_latest_device_values(["AMTI2","AMTI3"], ["Force"], ["Fz"])
-        if max(AMTI_vals) > self.ax.get_ylim()[1]:
-            self.ax.set_ylim(0, max(AMTI_vals))
 
+        # Set color. DoA of 0 = green, DoA of 1 = red
+        total_force = sum(AMTI_vals)
+        DoA = (AMTI_vals[1] - AMTI_vals[0])/total_force
+        rgb_colors = colors.hsv_to_rgb((115 - 115*abs(DoA))/255, 90/100, 90/100)
+
+        # Configure height of the graph to be the total force
+        self.ax.set_ylim(0, total_force)
+
+        # Update symmetry line height
+        self.symmline.set_ydata((total_force/2, total_force/2))
+        
         for bar, height in zip(self.bars, AMTI_vals):
             bar.set_height(height)
+            bar.set_color(rgb_colors)
         return self.bars
 
 
